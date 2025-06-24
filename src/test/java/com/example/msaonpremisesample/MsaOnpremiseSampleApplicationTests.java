76497d81f19eba3b8592b87dbd734f9cd1856587
soongonkim <soongon@gmail.com>
2025-06-24 15:27:15 +0900

    initial commit

---
+++ src/test/java/com/example/msaonpremisesample/MsaOnpremiseSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaonpremisesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaOnpremiseSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
